693972bf25f047db538b77305e99d24c62d34aab
crpgoncalves <carlosrpgoncalves@gmail.com>
2025-04-02 18:29:04 +0100

    xCode Project creation

---
+++ LebocoinChallenge/LebocoinChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LebocoinChallenge
+//
+//  Created by Carlos Gonçalves on 02/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LebocoinChallenge/LebocoinChallenge/LebocoinChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LebocoinChallengeApp.swift
+//  LebocoinChallenge
+//
+//  Created by Carlos Gonçalves on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LebocoinChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
