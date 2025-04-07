4297ffe99bf1b094103c6fd02ffd166ab91b1900
crpgoncalves <carlosrpgoncalves@gmail.com>
2025-04-07 16:20:28 +0100

    Remove Config.xcconfig

--- LeboncoinChallenge/LeboncoinChallenge/Network/NetworkConfigurationProvider.swift
+++ LeboncoinChallenge/LeboncoinChallenge/Network/NetworkConfigurationProvider.swift
@@ -14,16 +14,10 @@ protocol NetworkConfigurationProviding {
 struct NetworkConfigurationProvider: NetworkConfigurationProviding {
     
     var baseURL: URL {
-        if let urlString = ProcessInfo.processInfo.environment["BASE_URL"],
-           let url = URL(string: urlString) {
-            return url
-        }
-        
         if let urlString = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String,
            let url = URL(string: urlString) {
             return url
         }
-        
-        fatalError(" [NetworkConfigurationProvider] BASE_URL is missing or invalid in Build Settings, check your Config.xcconfig")
+        fatalError(" [NetworkConfigurationProvider] BASE_URL is missing or invalid in Build Settings, check your Info.plist")
     }
 }
